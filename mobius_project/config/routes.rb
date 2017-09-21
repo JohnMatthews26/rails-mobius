b263e363ccda1499c44af8515be38a6c29310ce2
John Matthews <johnmatthews2634@gmail.com>
2017-09-21 10:41:56 -0700

    add users model and controller, add sessions controller

--- mobius_project/config/routes.rb
+++ mobius_project/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :index, :show]
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: :show
+    end
+  end
 end
